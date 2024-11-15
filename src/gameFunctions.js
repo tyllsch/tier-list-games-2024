9756c3be06c442f90203fe489465fcccd26a1eb2
tyllsch <tyll.schmidt@gmail.com>
2024-11-15 01:40:27 +0100

    initialcommit

---
+++ src/gameFunctions.js
@@ -0,0 +1,66 @@
+// gameFunctions.js
+
+// Function to display the game summary
+function showSummary(image) {
+    const summaryDiv = document.getElementById('summary');
+    summaryDiv.style.display = 'block';
+
+    const summaryBackground = document.getElementById('summary-background');
+    const summaryTitle = document.getElementById('summary-title');
+    const summaryText = document.getElementById('summary-text');
+    
+    summaryTitle.textContent = image.getAttribute('data-title');
+    summaryText.innerHTML = image.getAttribute('data-summary');
+
+    const backgroundImage = image.getAttribute('data-img');
+    summaryBackground.src = backgroundImage;
+
+    summaryDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Function to add a new game card to the tier list
+function addGame(title, summary, imgTierList, imgSummaryCard, tier) {
+    let row;
+    switch (tier.toUpperCase()) {
+        case 'S':
+            row = document.querySelector('.label[style*="background-color:#FF3043"]').parentNode.querySelector('.games');
+            break;
+        case 'A':
+            row = document.querySelector('.label[style*="background-color:#FCA574"]').parentNode.querySelector('.games');
+            break;
+        case 'B':
+            row = document.querySelector('.label[style*="background-color:#D4D45E"]').parentNode.querySelector('.games');
+            break;
+        case 'C':
+            row = document.querySelector('.label[style*="background-color:#7D964C"]').parentNode.querySelector('.games');
+            break;
+        case 'D':
+            row = document.querySelector('.label[style*="background-color:#6BD5DA"]').parentNode.querySelector('.games');
+            break;
+        case 'F':
+            row = document.querySelector('.label[style*="background-color:#AD8DE0"]').parentNode.querySelector('.games');
+            break;
+        case 'DNF':
+            row = document.querySelector('.label[style*="background-color:#242424"]').parentNode.querySelector('.games');
+            break;
+        default:
+            console.error('Invalid tier');
+            return;
+    }
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.style.visibility = 'visible';
+
+    const img = document.createElement('img');
+    img.width = 90;
+    img.height = 90;
+    img.src = imgTierList;
+    img.setAttribute('data-title', title);
+    img.setAttribute('data-summary', summary);
+    img.setAttribute('data-img', imgSummaryCard);
+    img.setAttribute('onclick', 'showSummary(this)');
+
+    card.appendChild(img);
+    row.appendChild(card);
+}
---
+++ src/gameSetup.js
@@ -0,0 +1,195 @@
+// gameSetup.js
+
+window.onload = function() {
+    addGame(
+        'Hollow Knight', 
+        'Als ich angefangen habe Hollow Knight zu spielen war die erste Emotion, die ich dabei gefühlt habe, Frustration. Mir war direkt zum Start klar, dass das Spiel quasi für mich gemacht war und ich war enorm frustriert, es nicht viel früher gespielt zu haben. Umso besser, dass es dieses Jahr so weit war<br>Hollow Knight ist simpel. Nicht im Sinne, dass es Leicht wäre, aber dass alle Mechanics super intuitiv sind und die Perfektion und Kombination die größte Herausforderung ist.<br>Es gibt kein Aspekt, den ich als Schwäche ausmachen kann. Lore, World & Boss Design, Visuals, Kampf- & Skillsystem. Alles fühlt sich enorm optimiert an. Vor allem das Map Design. Es macht anfangs einen sehr linearen Eindruck aber irgendwann steht man fast ziellos rum. Aber schnell merkt man, dass jedes Ziel mehrere Wege hat und man für "Zielloses" Erkunden belohnt wird.<br>Anders als Viele fromsoft Games gibt es zudem einen Boss Replay Mode, und jedes "Souls-Like" verdient so etwas. Ist jetzt auch das zweite Jahr in Folge, dass ein Spiel mit Boss Replay Mode mein Spiel des Jahres wird<',
+        'img/cover/hollowknight.jpg', 
+        'img/sum/hollowknight.jpg', 
+        'S'
+    );
+	
+	addGame(
+		'Firewatch',
+		'Emotional, Visuell und von der Story ein wahnsinnig schönes Spiel. Aber mehr auch nicht.<br>Das Ende, Spoiler, hat mich ein bisschen enttäuscht und unterwältigt. Es passt wohl zum Motiv des Spiels, aber das Mysterium das Spiels war viel zu spannend für diese Konklusion.<br>Die Charaktere sind fantastisch geschrieben und vertont. Einer der besseren "Walking-Simulator" und ehrlich gesagt auch ein gutes Horror-Spiel. Zumindest fühlte es sich oft so an, ich weiß nicht, ob das so beabsichtigt war.',
+		'img/cover/firewatch.jpg',
+		'img/sum/firewatch.jpg',
+		'C'
+	);
+
+    addGame(
+        'Nine Sols', 
+        'Hab es direkt nach Hollow Knight begonnen und konnte mich nicht reinarbeiten, da es zu ähnlich war.<br>Ein Paar Monate später hab ich es lieben gelernt. Es ist offensichtlich von Sekiro und Hollow Knight inspiriert, weiß diese Inspiration aber auch umzusetzen, dass es ein spaßiges Kampfsystem ermöglicht.<br>Die Map, auch wenn anfangs unintuitiv, ist auch fantastisch gestaltet.<br>Visuell ist es wahrscheinlich das schönste Spiel was ich dieses Jahr gespielt habe. Die Farben, die handgezeichneten Paneele, alles pure Klasse<br>Die Story ist besser erzählt als Sekiro und Hollow Knight, emotional auch sehr viel ansprechender. Während ich Yi nicht immer mochte, war er trotzdem ein fantastischer Hauptcharakter. Meine Überraschung des Jahres, mein Lieblingsbossfight dieses Jahr.<br>Replayability ist leider eingeschränkt geil mit den vielen Cutscenes und keine Möglichkeit Bosse zu replayen.', 
+        'img/cover/ninesols.jpg', 
+        'img/sum/ninesols.jpg',
+        'S'
+    );
+
+    addGame(
+        'Cyberpunk Phantom Liberty', 
+        'Alles Cyberpunk bin ich total voreingenommen. Ein fantasischer DLC, der Cyberpunk Spy Thriller perfekt umsetzt. Manchmal zog es sich leicht, aber fantastisches Gameplay. Nur die Charaktere fand ich teilweise anstrengend, was mir auch leider das Ende etwas made gemacht hat, da ich nicht ganz den gewünschten emotionalen Zugang hatte.', 
+        'img/cover/cp_pl.jpg', 
+        'img/sum/cp_pl.jpg', 
+        'A'
+    );
+
+    addGame(
+        'Dredge', 
+        'Auch eine Überraschung für mich. Lovecraftian Horror ist geil und dieses Spiel vereint "Cozy-Game" und existentielle Angst perfekt.<br>Das Spiel hatte leichte Pacing Probleme, da nach ca. 50% wenig neue Mechanics hinzukommen und man nur noch die Story durchrusht aber vllt. hab ich am Anfang auch zu viel gegrindet.<br>Story war fantastisch, das Spiel hat wahrscheinlich, dicker Spoiler an der Stelle, den besten Plot Twist in einem Videospiel, den ich jemals gesehen hab.', 
+        'img/cover/dredge.jpg', 
+        'img/sum/dredge.jpg', 
+        'A'
+    );
+
+    addGame(
+        'Ghost Trick: Phantom Detective', 
+        'Ich liebe Puzzlespiele mit Twists. Den Core Gameplay Loop von Ghost Trick muss man mögen, sonst hat man keinen Spaß, aber bei mir hat er auf jeden Fall gezündet.<br>Die Story und Charakter sind fantastisch geschrieben, dass man auch jenseits des normalen Bedürfnis, das Spiel durchzuspielen, ständig motiviert ist, die übergeordnete Story zu lössen.', 
+        'img/cover/ghosttrick.jpg', 
+        'img/sum/ghosttrick.jpg', 
+        'A'
+    );
+
+    addGame(
+        'Signalis', 
+        'Survival Horror a la Silent Hill sind auch nicht mein Ding (Cyberpunk schon). Dafür hab ich dieses Spiel krass lieben gelernt.<br>Das Spiel wirkt super optimiert, alle Mechanics funktioneren klasse zusammen und während ich die Story auch nach ganz vielen Youtube Sessions nicht ganz verstanden hab, sind die Motive des Spiels mir überraschend nahe gekommen.', 
+        'img/cover/signalis.jpg', 
+        'img/sum/signalis.jpg',
+        'B'
+    );
+
+    addGame(
+        "Tony Hawk's Underground", 
+        'Die Tony Hawk Reihe war ein Pfeiler meiner Kindheit. Dieser Teil ist mir aber immer entwichen, einfach weil meine Eltern mir ihn nie gekauft haben. Der Story Mode ist einer der bersten der Reihe und mal was komplett neues.<br>Als THUG2 Fan war das Gameplay nichts neues/überraschendes aber es hat einfach wahnsinnig spaß gemacht.<br>Fick Eric Sparrow.', 
+        'img/cover/thug.jpg', 
+        'img/sum/thug.jpg', 
+        'B'
+    );
+
+    addGame(
+        'OlliOlli World', 
+        '', 
+        'img/cover/olliolli.jpg', 
+        'img/sum/olliolli.jpg', 
+        'B'
+    );
+
+    addGame(
+        'Burnout Paradise', 
+        '', 
+        'img/cover/burnoutparadise.jpg', 
+        'img/sum/burnoutparadise.jpg', 
+        'C'
+    );
+
+    addGame(
+        'Duck Detective: The Secret Salami', 
+        '', 
+        'img/cover/duckdetective.jpg', 
+        'img/sum/duckdetective.jpg', 
+        'C'
+    );
+
+    addGame(
+        'Dead by Daylight', 
+        'Asymettrischer Mulitplayer ist immer eine Herausforderung, Dead By Daylight macht das aktuell glaube ganz gut.<br>Die Runden machen spaß, auch wenn man verliert, und es wirkt insgesamt einfach wie ein super Spiel, dass man mit Friends zocken kann. Muss ich häufiger spielen', 
+        'img/cover/dbd.jpg', 
+        'img/sum/dbd.jpg', 
+        'C'
+    );
+
+    addGame(
+        'Pony Island', 
+        'Solide. Ich hab Inscryption vor diesem Spiel gespielt und demnach hohe Erwartungen gehabt. Man sieht offensichtlich ähnliche Konzepte, wie bei Inscryption, aber alles fühlte sich etwas unausgereifter an.<br>Das Core Gameplay macht spaß und auch die Story war gut, aber ich merkte, wie mir nichts davon länger im Kopf blieb. Ich kann nichtmal Kritik formulieren, das Spiel war einfach... ganz gut?', 
+        'img/cover/ponyisland.jpg', 
+        'img/sum/ponyisland.jpg', 
+        'D'
+    );
+
+    addGame(
+        'Resident Evil 2', 
+        'Hier kann ich mich kurz und knackig halten. Nicht meine Genre, hatte trotzdem Spaß.<br>Vor allem die Metroidvania Komponenten fand ich klasse. Die Map zu lernen hat sich gut und intuitiv angefühlt. Wünschte, es hätte noch mehr Content im RCPD gegeben.<br>Weiß nicht, ob ich den Nemesis mochte, da ich vor allem die Erkundung der Polizeistation gut fand und die nach seiner Einführung eher rückgestellte Prio eingenommen hat.', 
+        'img/cover/re2.jpg', 
+        'img/sum/re2.jpg',
+        'D'
+    );
+
+    addGame(
+        'Balatro', 
+        'Roguelite. Hätte ich das mal früher gewusst. Insgesamt besser als ein Otxo, optisch auch mein Ding.<br>Aber mehr als witzige Unterhaltung nebenbei ist das Spiel für mich auch nicht. Man merkt dem Spiel aber Qualität an und ich hatte eigentlich immer Spaß', 
+        'img/cover/balatro.jpg', 
+        'img/sum/balatro.jpg',
+        'B'
+    );
+
+    addGame(
+        'Palworld', 
+        'Survival Games sind ein Genre, das für mich relativ Hit or Miss ist. Grounded bspw. ist fantastisch und macht vieles richtig, während ARK für mich z.B. einfach überladen und aufgeblasen ist.<br>Palworld fällt eher in die letztere Kategorie. Abseits der offensichtlichen Pokémon "Inspiration" merkt man, dass das Spiel nahezu keine Survival Mechanics neu erfindet.<br>Insgesamt unglaublich langweilig', 
+        'img/cover/palworld.jpg', 
+        'img/sum/palworld.jpg', 
+        'F'
+    );
+
+    addGame(
+        'Otxo', 
+        'Eine wertvolle Entdeckung, die ich dieses Jahr gemacht habe ist, dass Roguelites nichts für mich sind<br>Das Spiel nimmt viele Aspekte von Hotline Miami, die ich geil fand, und schafft es trotzdem sich unglaublich unzufriedenstellend zu spielen.<br>Hatte das Gefühl, viele Mechanics konkurrieren gegeneinander. Bspw. ist es schnell und adrenalingeladen, aber hat Bullet Time und nimmt damit die Geschwindigkeit aus dem Spiel.<br>Hab nicht zu Ende gespielt und werde das auch nicht nachholen. Packt mich einfach nicht, ist aber wahrscheinlich ganz gut.',
+        'img/cover/otxo.jpg', 
+        'img/sum/otxo.jpg', 
+        'DNF'
+    );
+
+    addGame(
+        'Catherine', 
+        'Die Story und Themes fand ich eigentlich super, aber das Gameplay war absolut nicht mein Fall.<br>Die Puzzles ziehen sich und sind unglaublich repetitiv und langweilig. Story war gut genug, dass ich mich danach eingelesen hab.', 
+        'img/cover/catherine.jpg', 
+        'img/sum/catherine.jpg', 
+        'DNF'
+    );
+
+    addGame(
+        'Chants of Sennaar',
+        '',
+        'img/cover/chantsofsennaar.jpg',
+        'img/sum/chantsofsennaar.jpg',
+        'B'
+    );
+
+    addGame(
+        'The Dark Pictures: House of Ashes',
+        '',
+        'img/cover/hoa.jpg',
+        'img/sum/hoa.jpg',
+        'D'
+    );
+
+    addGame(
+        'The Henry Stickmin Collection',
+        '',
+        'img/cover/henrystickmin.jpg',
+        'img/sum/henrystickmin.jpg',
+        'D'
+    );
+    
+    addGame(
+        'Dave The Diver',
+        '',
+        'img/cover/dave.jpg',
+        'img/sum/dave.jpg',
+        'DNF'
+    )
+
+    addGame(
+        'Dishonored 2',
+        '',
+        'img/cover/dishonored2.jpg',
+        'img/sum/dishonored2.jpg',
+        'A'
+    )
+
+    addGame(
+        'Citizen Sleeper',
+        '',
+        'img/cover/citizensleeper.jpg',
+        'img/sum/citizensleeper.jpg',
+        'DNF'
+    )
+};
